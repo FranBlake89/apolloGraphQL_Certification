645f45a2cd310fdd2bf501bde8781773106c2592
Francisco Castillo <73005797+FranBlake89@users.noreply.github.com>
2023-10-28 23:14:05 -0400

    update schema

--- odyssey-lift-off-part3/server/src/schema.js
+++ odyssey-lift-off-part3/server/src/schema.js
@@ -19,6 +19,20 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "THe track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]! 
+  }
+
+"A module is a single unit of teaching. Multiple modules compose a track"
+  type Module{
+    id: ID!
+    title: String!
+    "The module's length in minutes"
+    length: Int
   }
 
   "Author of a complete Track or a Module"
